e5953dbab75d04d795c01f62d6ece4c0a217c7c5
birds0416 <68123073+birds0416@users.noreply.github.com>
2021-02-04 05:26:07 +0900

    Update luggage_location.py

--- luggage_location.py
+++ luggage_location.py
@@ -2,8 +2,9 @@
 # 수취대 위 수하물 정보에 대한 qr코드 스캔은 아두이노를 사용하여 스캔
 # 제작자: 임호정 birds8277@gmail.com
 
-import math
 import time
+import math
+
 
 # 인천공항 수하물 수취대 기준, 가로 길이가 15m 로 추정, 한바퀴 도는 데에 5분이라고 추정
 # 모형 수취대의 가로 길이 = 0.5m, 너비 = 0.15m
@@ -27,6 +28,23 @@ class Luggage:
         return '(' + self.info + ', ' + self.location + ')'
     
 # end of class luggage
+
+class StopWatch:
+    
+    def __init__(self):
+        self.start_time = 0
+        self.stop_time = 0
+        
+    def start(self):
+        self.start_time = time.time()
+        return self.start_time
+
+    def stop(self):
+        self.stop_time = time.time()
+        return self.stop_time
+    
+    def get_elapsed(self):
+        return self.stop_time - self.start_time
     
         
 total_length = 50 #in cm
@@ -35,6 +53,7 @@ sections = {'A' : 0, 'B': 10, 'C' : 20, 'D' : 30, 'E' : 40}
 
 def calculate_distance(scan_info):  #수하물의 qr 코드 정보를 parameter로 받아야함
     
+    time = StopWatch()
     l1 = Luggage(scan_info)
     
     
